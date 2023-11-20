4ff45c97dcc0b51b10b2257acc6a1564c6ee957a
Jordan <jofre55@ulaval.ca>
2023-11-20 13:28:08 -0500

    correction esthétique du code

--- phase1.py
+++ phase1.py
@@ -3,14 +3,27 @@ import argparse
 import datetime
 import requests
 
+
 #ceci est l'initialisation de mon parser qui gère les arguments du module.
-parser = argparse.ArgumentParser(description='Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.')
+parser = argparse.ArgumentParser(
+    description='Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.')
 
 #ceci est la liste des argument qu'accept le module lors de l'appel, sybole est le seul obligatoire.
 parser.add_argument('symbole', type=str, help='Nom d un symbole boursier')
-parser.add_argument('-d', '--début', metavar='DATE', type=str, help='date rechercher la plus ancienne(format: AAAA-MM-JJ)')
-parser.add_argument('-f', '--fin', metavar='DATE', type=str, default=str(datetime.date.today()), help='date rechercher la plus ancienne(format: AAAA-MM-JJ)')
-parser.add_argument('-v', '--valeur', metavar='{fermeture,ouverture,min,max,volume}', type=str, default='fermeture',  help='La valeur désiré(par défaut: fermeture)')
+parser.add_argument('-d', '--début', 
+                    metavar='DATE', 
+                    type=str, 
+                    help='date rechercher la plus ancienne(format: AAAA-MM-JJ)')
+parser.add_argument('-f', '--fin', 
+                    metavar='DATE', 
+                    type=str, 
+                    default=str(datetime.date.today()),
+                    help='date rechercher la plus ancienne(format: AAAA-MM-JJ)')
+parser.add_argument('-v', '--valeur', 
+                    metavar='{fermeture,ouverture,min,max,volume}', 
+                    type=str, 
+                    default='fermeture', 
+                    help='La valeur désiré(par défaut: fermeture)')
 #ici un objet args est créé contenant la liste des entré lors de l'appel du module.
 args=parser.parse_args()
 
@@ -20,16 +33,17 @@ v = args.valeur
 d = args.début
 f = args.fin
 
+
 #méthode permettant de transformer argument de la méthode parse_args en un affichage désiré
 def analyser_commande(s, v, d, f):
     if d == None:
         d = f
-        print(d)
-    pass
-    return ('titre={}: valeur={}, début=datetime.date({}), fin=datetime.date({})'.format(s, v, d.replace('-',', '), f.replace('-',', ')))
+    return ('titre={}: valeur={}, début=datetime.date({}), fin=datetime.date({})'
+            .format(s, v, d.replace('-',', '), f.replace('-',', ')))
 
 #affichage à la console des commandes donné fournie au module   
 print(analyser_commande(s, v, d, f))
+
 
 def produire_historique(s, v, d, f):
     if d == None:
@@ -41,15 +55,18 @@ def produire_historique(s, v, d, f):
     réponse = requests.get(url=url, params=params)
     réponse = json.loads(réponse.text)
     réponse = réponse['historique']
+    
     t = ()
     h = []
     for clé in réponse.keys():
         date = f'datetime.date({clé.replace('-',', ')})'
         t += (date, réponse[clé][v])
+    
     l = len(t)
     for i in range(0,l,2):
         h.append(t[i:i+2])
     h.sort()
+
     return h
 
 #affichage à la consol de l'historique demandé     
