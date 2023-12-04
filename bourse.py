9c93bda51bb2bb1987df19a0bf3ce7b555e4dc06
Jordan <jofre55@ulaval.ca>
2023-12-03 22:43:57 -0500

    class bourse

---
+++ bourse.py
@@ -0,0 +1,19 @@
+class Bourse:
+    
+#cette méthode doit retourner le prix à la fermeture
+    def prix(symbole, date):
+        import datetime
+        if date > datetime.date.today():
+            raise ValueError
+        if date == datetime.date.today():
+            date = date - datetime.timedelta(days = 1)
+        
+        from phase1 import produire_historique
+        
+        x = produire_historique(symbole, 'fermeture', date, date)
+        if x == []:
+            while x == []:
+                date = date - datetime.timedelta(days = 1)
+                x = produire_historique(symbole, 'fermeture', date, date)
+        return float(x[0][1])
+
---
+++ portefeuille.py
@@ -0,0 +1,7 @@
+class Portefeuille:
+
+    def __init__(self, x):
+        self.x = x
+
+    def déposer(self, montant, date):
+        
